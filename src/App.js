6243db87eac79f23b1d8d4ba474c185827a9aaa4
Cale Haug <caleahaug@gmail.com>
2020-07-25 14:17:56 -0700

    good enough for the moment i think

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <img src={logo} className="logo" alt="BlackBall Logo" />
-        {/* <h1>BlackBall.co/</h1> */}
-        <hr />
-        <h2>A Blockchain solution to tracking law enforcement civil rights abuses.  Verified abuse of power complaints would be added to a blockchain, which may be freely searched by anyone.</h2>
-        <br />
-        <h3>A Blackball for the Blue Wall of Silence.</h3>
-        <br />
-        <h3>🚧👷🏗️ Site Under Construction 🏗️👷🚧</h3>
-        <br />
-  <h3>©{new Date().getUTCFullYear()} <a href="https://github.com/cahaug/BlackBall">BlackBall.co/</a></h3>
+    <div className="wrapper">
+      <div className="App">
+          <img src={logo} className="logo" alt="BlackBall Logo" />
+          {/* <h1>BlackBall.co/</h1> */}
+          <hr />
+          <h2>A Blockchain solution to tracking law enforcement civil rights abuses.  Verified abuse of power complaints would be added to a blockchain, which may be freely searched by anyone.</h2>
+          <br />
+          <h3>A Blackball for the Blue Wall of Silence.</h3>
+          <br />
+          <h3>🚧👷🏗️ Site Under Construction 🏗️👷🚧</h3>
+          <br />
+          <h3>©{new Date().getUTCFullYear()} <a href="https://github.com/cahaug/BlackBall">BlackBall.co/</a></h3>
+      </div>
     </div>
   );
 }
