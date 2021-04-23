a7045197ea1042124ecb373b350fea1ce2041f81
Cale Haug <caleahaug@gmail.com>
2021-04-23 02:59:54 -0700

    fleshing out evidence endpoints

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './files/blackballIMG.png';
 import './App.css';
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <div className="wrapper">
+      <Toaster />
       <div className="App">
           <img src={logo} className="logo" alt="BlackBall Logo" />
           {/* <h1>BlackBall.co/</h1> */}
           <hr />
-          <h2>A Blockchain solution to tracking law enforcement civil rights abuses.  Verified abuse of power complaints would be added to a blockchain, which may be freely searched by anyone.</h2>
+          <h2>A Blockchain solution to tracking abusers.  Verified abusers are added to a blockchain, which may be freely searched by anyone.</h2>
           <br />
-          <h3>A Blackball for the Blue Wall of Silence.</h3>
+          <h3>A Blackball for the Blue Wall of Silence, and more.</h3>
           <br />
           <h3>🚧👷🏗️ Site Under Construction 🏗️👷🚧</h3>
           <br />
-          <h3>©{new Date().getUTCFullYear()} <a href="https://github.com/cahaug/BlackBall">BlackBall.co/</a></h3>
+          <h3>©{new Date().getUTCFullYear()} <a href="https://blackball.co/">K.P. - BlackBall - Hecatomb - Oblation Corporation (K.P.B.H.O.C.)</a></h3>
       </div>
     </div>
   );
