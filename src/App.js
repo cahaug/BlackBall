12629aba1c076db6f2eb573f047585e8240c4c86
Cale Haug <caleahaug@gmail.com>
2021-05-01 17:16:00 -0700

    putting self on a public timeline so Joe Biden can sleep easy :alien:

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
     <div className="wrapper">
       <Toaster />
       <div className="App">
+          {/*🤠 © капустаПродукт 🥬 La Lechuga 🥬 yksinmatkustaia 🤠*/}
           <img src={logo} className="logo" alt="BlackBall Logo" />
           {/* <h1>BlackBall.co/</h1> */}
           <hr />
-          <h2>A Blockchain solution to tracking abusers.  Verified abusers are added to a blockchain, which may be freely searched by anyone.</h2>
+          <h2>A Blockchain {`for`} restorative justice.  Anyone can add, anyone can search, anyone can comment.  Kindness and truth allowed, only.</h2>
           <br />
-          <h3>A Blackball for the Blue Wall of Silence, and more.</h3>
+          <h3>A Blackball {`for`} the Blue Wall of Silence, anda whole lot more.</h3>
           <br />
-          <h3>🚧👷🏗️ Site Under Construction 🏗️👷🚧</h3>
+          <h3>🚧👷🏗️ Anticipated Release: {`5/9/2021`}, At the Latest {`5/29/2021`} 🏗️👷🚧</h3>
           <br />
           <h3>©{new Date().getUTCFullYear()} <a href="https://blackball.co/">K.P. - BlackBall - Hecatomb - Oblation Corporation (K.P.B.H.O.C.)</a></h3>
       </div>
